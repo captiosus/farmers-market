14c39456ec469a54606029bfc33a2c32bc3aca8a
omgimanerd <alvin.lin.dev@gmail.com>
2016-03-28 11:38:49 -0400

    base files

---
+++ server.js
@@ -0,0 +1,63 @@
+/**
+ * @fileoverview This is the server app script.
+ * @author alvin.lin.dev@gmail.com (Alvin Lin)
+ */
+
+// Constants
+var DEV_MODE = false;
+var IP = process.env.IP || 'localhost';
+var PORT_NUMBER = process.env.PORT || 5000;
+
+// Sets the DEV_MODE constant during development if we run 'node server --dev'
+process.argv.forEach(function(value, index, array) {
+  if (value == '--dev' || value == '--development') {
+    DEV_MODE = true;
+  }
+});
+
+// Dependencies.
+var assert = require('assert');
+var bodyParser = require('body-parser');
+var express = require('express');
+var http = require('http');
+var morgan = require('morgan');
+var session = require('express-session');
+var mongodb = require('mongodb');
+
+var AccountManager = require('./lib/AccountManager');
+
+// Initialization.
+var app = express();
+var server = http.Server(app);
+var acm = AccountManager.create();
+
+app.set('port', PORT_NUMBER);
+app.set('view engine', 'jade');
+
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(morgan(':date[web] :method :url :req[header] :remote-addr :status'));
+app.use('/public',
+        express.static(__dirname + '/public'));
+app.use('/shared',
+        express.static(__dirname + '/shared'));
+
+// Use request.query for GET request params.
+// Use request.body for POST request params.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(request, response) {
+  response.render('index');
+})
+
+// Starts the server.
+server.listen(PORT_NUMBER, function() {
+  console.log('STARTING SERVER ON PORT ' + PORT_NUMBER);
+  if (DEV_MODE) {
+    console.log('DEVELOPMENT MODE ENABLED');
+  }
+});
