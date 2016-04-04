e243746c3c8ed701b20f25ccff149e0ae787e163
captiosus <royxu652@yahoo.com>
2016-04-03 21:04:18 -0400

    stars

--- public/js/profile.js
+++ public/js/profile.js
@@ -10,3 +10,14 @@ $('.review-list').bind('mousewheel', function (e) {
       return false;
     }
 });
+var rate = document.querySelector('#rating');
+var rate1 = document.querySelector('#rating1');
+var rate2 = document.querySelector('#rating2');
+var rate3 = document.querySelector('#rating3');
+var currentRating = 0;
+var maxRating= 5;
+var callback = function(rating) {  };
+var myRating = rating(rate, currentRating, maxRating, callback);
+var myRating1 = rating(rate1, currentRating, maxRating, callback);
+var myRating2 = rating(rate2, currentRating, maxRating, callback);
+var myRating3 = rating(rate3, currentRating, maxRating, callback);
